ae1c64901d4fc7cb8a27ee64ba27835102c23c4a
Dinislam2009 <dinislamjumamuratov18@gmail.com>
2026-08-18 14:56:12 +0500

    .

--- middleware.ts
+++ middleware.ts
@@ -40,6 +40,3 @@ export function middleware(request) {
 export const config = {
   matcher: ["/((?!_next|api|favicon.ico).*)"],
 };
-export default function middleware(request: Request) {
-  // middleware коды
-}
